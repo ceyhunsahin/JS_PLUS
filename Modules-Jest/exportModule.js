7b9721b86e3919ed0a3b7c8f707349600573481b
ceyhunsahin <csahin17072010@gmail.com>
2023-12-23 12:57:58 +0100

    Modules

---
+++ Modules-Jest/exportModule.js
@@ -0,0 +1,16 @@
+export const method1 = (num) => {
+    return num * num;
+}
+
+export const SECOND = 60;
+const DAY = "Mercredi";
+const MONTHS = "December" ;
+
+export { DAY, MONTHS as moi };
+
+
+function add (n1, n2) {
+    return n1 + n2
+}
+
+export {add}
---
+++ Modules-Jest/importModule.js
@@ -0,0 +1,9 @@
+import { method1, DAY as d, moi, add  } from './exportModule.js';
+
+console.log(method1(6));
+
+console.log(d);
+
+console.log(moi);
+
+console.log(add(4,9));
